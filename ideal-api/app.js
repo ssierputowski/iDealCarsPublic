937712f59be8305e3e550e45fd9e0872f70c7746
Niraj Patel <31775664+nirajp23@users.noreply.github.com>
2018-10-23 21:16:50 -0400

    sagar fix it

--- ideal-api/app.js
+++ ideal-api/app.js
@@ -8,5 +8,3 @@ const mongoose = require('mongoose');
 // Eric was up in here!
 // Niraj Was here
 // Sagar is making a test comment!!
-// My comment wasn't good enough -stash
-// Editing Jerrod's comment.
