59aa800557eacefb4d5401bc11fbffe018a0acce
Niraj Patel <31775664+nirajp23@users.noreply.github.com>
2018-10-23 20:53:00 -0400

    conflict

--- ideal-api/app.js
+++ ideal-api/app.js
@@ -2,10 +2,3 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-// Server logic for the app
-
-// Test comment for commiting to branch
-// Eric was up in here!
-
-// Sagar is making a test comment!!
-// My comment wasn't good enough -stash
