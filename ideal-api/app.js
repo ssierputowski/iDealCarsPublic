24bb0626621f309076e3f3f892a0f528583c55b1
Jerrod Mathis <jerrodmathis95@gmail.com>
2018-10-23 20:51:30 -0400

    creating conflict

--- ideal-api/app.js
+++ ideal-api/app.js
@@ -4,9 +4,4 @@ const mongoose = require('mongoose');
 
 // Server logic for the app
 
-// Test comment for commiting to branch
-// Eric was up in here!
-// Niraj Wasn't here
-// Sagar is making a test comment!!
-// My comment wasn't good enough -stash
-// Editing Jerrod's comment.
+// Trying to create merge conflict
