682aa22f115a15aa3a63dae13521d7fc71234427
Sagar Patadia <35351813+spatadia@users.noreply.github.com>
2018-10-23 21:12:24 -0400

    Commit

--- ideal-api/app.js
+++ ideal-api/app.js
@@ -1,15 +1,6 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-<<<<<<< HEAD
-=======
+const bodyParser = require('body-parser');
 
-// Server logic for the app
 
-// Test comment for commiting to branch
-// Eric was up in here!
-// Niraj Was here
-// Sagar is making a test comment!!
-// My comment wasn't good enough -stash
-// Editing Jerrod's comment.
->>>>>>> aaa97fc370db922d2079ba80af3fc088058b1644
+// I am making a conflict
