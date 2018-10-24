55dde5ea2ca1c2ebc7b14c6087a9e054984d0d40
Sagar Patadia <35351813+spatadia@users.noreply.github.com>
2018-10-23 20:50:03 -0400

    Changing comment

--- ideal-api/app.js
+++ ideal-api/app.js
@@ -4,9 +4,4 @@ const mongoose = require('mongoose');
 
 // Server logic for the app
 
-// Test comment for commiting to branch
-// Eric was up in here!
-// Niraj Was here
-// Sagar is making a test comment!!
-// My comment wasn't good enough -stash
-// Editing Jerrod's comment.
+// Change comment
