6af690c824b27e164c63e0f5839063879592b6b3
ewdavies <42724525+ewdavies@users.noreply.github.com>
2018-10-23 21:09:31 -0400

    new conflict

--- ideal-api/app.js
+++ ideal-api/app.js
@@ -2,11 +2,4 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-// Server logic for the app
-
-// Test comment for commiting to branch
-// Eric was up in here!
-// Niraj Was here
-// Sagar is making a test comment!!
-// My comment wasn't good enough -stash
-// Editing Jerrod's comment.
+// Hey this is new, Find my mistake
