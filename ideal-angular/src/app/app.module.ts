166ec6b658b155f994aedb42c76f8d6d5d83f832
Stash Sierputowski <stashsierputowski@Stashs-MacBook-Pro.local>
2018-10-29 14:41:14 -0400

    prettierish login and start on home

--- ideal-angular/src/app/app.module.ts
+++ ideal-angular/src/app/app.module.ts
@@ -5,15 +5,18 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent},
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
