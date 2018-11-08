f886c0372f34278f71c29286fd2bcc93b77bc6ee
Jerrod Mathis <jerrodmathis95@gmail.com>
2018-11-08 15:56:26 -0500

    adding server file, updating app.js, adding gitignore for project

--- ideal-api/app.js
+++ ideal-api/app.js
@@ -2,12 +2,30 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-// Server logic for the app
+const app = express();
 
+mongoose.connect('mongodb://jmathis:ePA0FmXpjtnlzcji@ideal-cars-shard-00-00-szrks.mongodb.net:27017,ideal-cars-shard-00-01-szrks.mongodb.net:27017,ideal-cars-shard-00-02-szrks.mongodb.net:27017/test?ssl=true&replicaSet=ideal-cars-shard-0&authSource=admin&retryWrites=true', { useMongoClient: true })
+    .then(() => {
+        console.log('Connected to the database!');
+    })
+    .catch(() => {
+        console.log('Connection failed!');
+    });
 
-// Test comment for commiting to branch
-// Eric was up in here!
-// Niraj Was here
-// Sagar is making a test comment!!
-// My comment wasn't good enough -stash
-// Editing Jerrod's comment.
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PATCH, DELETE, OPTIONS'
+    );
+    next();
+});
+
+module.exports = app;
