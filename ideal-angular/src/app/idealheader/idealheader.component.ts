3ecdebdbe4069ed4b5581b5490e20da6f46fc5b2
ewdavies <42724525+ewdavies@users.noreply.github.com>
2018-11-13 19:51:30 -0500

    firstCommit

---
+++ ideal-angular/src/app/idealheader/idealheader.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-idealheader',
+  templateUrl: './idealheader.component.html'
+
+})
+export class HeaderComponent {}
---
+++ ideal-angular/src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { CdkScrollable } from '@angular/cdk/overlay';
+
+@Component ({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html'
+})
+export class SidebarComponent {}
