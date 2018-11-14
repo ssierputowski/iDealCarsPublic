16592d845bc4b1671f9391595e11f349383c11e8
Sagar Patadia <35351813+spatadia@users.noreply.github.com>
2018-11-13 19:20:08 -0500

    Commit

--- ideal-api/app.js
+++ ideal-api/app.js
@@ -2,9 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-// This file is the server logic for the app
 
-// Test comment for commiting to branch
-// Eric was up in here!
-// Niraj Was here
-// Sagar is making a test comment!!
+// I am making a conflict
+
+// This file is the server logic for the app
