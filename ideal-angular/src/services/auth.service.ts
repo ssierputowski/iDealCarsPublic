cc1f7153e6f7ee4ba55101aa9fe02a8e3775bca0
Jerrod Mathis <jerrodmathis95@gmail.com>
2018-11-19 08:40:28 -0500

    finished login, just need to make minor changes to complete the login

--- ideal-angular/src/services/auth.service.ts
+++ ideal-angular/src/services/auth.service.ts
@@ -1,17 +1,49 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 import { User } from '../models/user.model';
+import { Subject } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
-  constructor(private http: HttpClient) { }
+
+  private isAuthenticated = false;
+  private token: string;
+  private authStatusListener = new Subject<boolean>();
+
+  constructor(private http: HttpClient, private router: Router) { }
+
+  getToken() {
+    return this.token;
+  }
+
+  getIsAuth() {
+    return this.isAuthenticated;
+  }
+
+  getAuthStatusListener() {
+    return this.authStatusListener.asObservable();
+  }
 
   login(username: string, password: string) {
     const userData: User = { username: username, password: password };
-    this.http.post('http://localhost:3000/api/user/login', userData)
+    this.http.post<{token: string}>('http://localhost:3000/api/user/login', userData)
       .subscribe(res => {
-        console.log(res);
+        const token = res.token;
+        this.token = token;
+        if (token) {
+          this.isAuthenticated = true;
+          this.authStatusListener.next(true);
+          this.router.navigate(['/home']);
+        }
       });
+  }
+
+  logout() {
+    this.token = null;
+    this.isAuthenticated = false;
+    this.authStatusListener.next(false);
+    this.router.navigate(['/']);
   }
 }
