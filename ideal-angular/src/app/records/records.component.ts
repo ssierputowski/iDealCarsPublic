7a9356fc7797a431262b9fd8226a73b8dae7896c
Sagar Patadia <35351813+spatadia@users.noreply.github.com>
2018-12-03 16:00:37 -0500

    Deleted js files and jquery from package lock

--- ideal-angular/src/app/records/records.component.ts
+++ ideal-angular/src/app/records/records.component.ts
@@ -18,8 +18,24 @@ export class RecordsComponent implements OnInit {
       isFirstOpen: true,
       isFirstDisabled: false
     };
-  }
+    $scope.newContacts = {};
+    $scope.saved = localStorage.getItem('contacts');
+    $scope.contacts = (localStorage.getItem('contacts') !== null) ? JSON.parse($scope.saved) : [
+    {fname: 'Sagar', lname: 'Patadia', carYear: '2019', carMake: 'Mercedes',
+    carModel: 'C300', telephone: '0123456789', email: 'sagar@idealcars.com'}
+    ];
 
+    localStorage.setItem('contacts', JSON.stringify($scope.contacts));
+
+    $scope.saveContact = function() {
+
+      $scope.contacts.push($scope.newContacts);
+      localStorage.setItem('contacts', JSON.stringify($scope.contacts));
+      $scope.newContacts = {};
+
+    };
+
+  }
 
 }
 /**
