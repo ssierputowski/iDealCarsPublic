bc62f0069adaa4824e276b85fee0214afd9cd207
Jerrod Mathis <jerrodmathis95@gmail.com>
2018-12-04 20:24:51 -0500

    updating header component

--- ideal-angular/src/app/app.module.ts
+++ ideal-angular/src/app/app.module.ts
@@ -23,7 +23,7 @@ import { TimeClockComponent } from './time-clock/time-clock.component';
 import { InventoryComponent } from './inventory/inventory.component';
 
 const routes: Routes = [
-  { path: '', component: LoginComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent },
   { path: 'records', component: RecordsComponent},
