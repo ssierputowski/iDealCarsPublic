4f37325e2e9bbf8427e787905c8be7223c67e409
ewdavies <42724525+ewdavies@users.noreply.github.com>
2018-12-06 23:48:41 -0500

    Committing for fresh pull

--- ideal-angular/src/app/app.module.ts
+++ ideal-angular/src/app/app.module.ts
@@ -9,7 +9,8 @@ import {
   MatListModule,
   MatIconModule,
   MatCardModule,
-  MatSidenavModule
+  MatSidenavModule,
+MatExpansionModule,
 } from '@angular/material';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -56,6 +57,7 @@ const routes: Routes = [
     MatCardModule,
     MatIconModule,
     MatSidenavModule,
+    MatExpansionModule,
     RouterModule.forRoot(routes),
   ],
   providers: [],
