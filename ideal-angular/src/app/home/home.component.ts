b819573e90ff6baa0b5ad337930be887416ee860
Stanley Sierputowski <sierputs@email.sc.edu>
2019-02-14 16:55:34 -0500

    git blows my mind but i think i've figured it out (ignore)

--- ideal-angular/src/app/home/home.component.ts
+++ ideal-angular/src/app/home/home.component.ts
@@ -2,34 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { Router } from '@angular/router';
 
-import { LeadService } from 'src/services/lead.service';
-import { Lead } from '../../models/lead.model';
-import { Subscription } from 'rxjs';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  isFirstOpen = true;
-  showFiller = false;
 
-  leads: Lead[] = [];
-  totalLeads = 0;
-  private leadsSub: Subscription;
-
-  isLoading = false;
-
-  form: FormGroup;
-
-  constructor(
-    private router: Router,
-    private titleService: Title,
-    private leadService: LeadService
-    ) { }
+  constructor(private router: Router, private titleService: Title) { }
 
   // User variables
   // Fetch from database
@@ -40,34 +20,6 @@ export class HomeComponent implements OnInit {
   jobRole = 'Salesman';
 
   ngOnInit() {
-    this.isLoading = true;
     this.titleService.setTitle('Home | iDealCars');
-
-    this.form = new FormGroup({
-      'email': new FormControl(null, {
-        validators: [Validators.required]
-      }),
-      'name': new FormControl(null, {
-        validators: [Validators.required]
-      }),
-      'number': new FormControl(null, {
-        validators: [Validators.required]
-      }),
-      'comment': new FormControl(null, {
-        validators: [Validators.required]
-      }),
-      'car': new FormControl(null, {
-        validators: [Validators.required]
-      }),
-      'employee': new FormControl(null, {
-        validators: [Validators.required]
-      }),
-      'id': new FormControl(null, {
-        validators: [Validators.required]
-      })
-    });
-    this.leadService.getLeads();
-    // this.leadsSub = this.leadService.getLeadUpdateListener()
-      // .subscribe((leadData))
   }
 }
--- ideal-angular/src/models/lead.model.ts
+++
@@ -1,8 +0,0 @@
-export interface Lead {
-    email: string;
-    name: string;
-    phone: string;
-    comment: string;
-    car: string;
-    employee: string;
-}
