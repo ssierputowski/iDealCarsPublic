49a0011797f7246270001353a3b57a174299e756
Sagar Patadia <35351813+spatadia@users.noreply.github.com>
2019-02-24 21:23:35 -0500

    Pulling Eric's to try the form data thing

--- ideal-angular/src/app/app.module.ts
+++ ideal-angular/src/app/app.module.ts
@@ -12,19 +12,14 @@ import {
   MatSidenavModule,
   MatExpansionModule,
   MatDividerModule,
-<<<<<<< HEAD
   MatTableModule,
   MatRadioModule,
   MatDialogModule,
   MatTabsModule
-=======
-  MatRadioModule
->>>>>>> 254234dc2bfd279d77a67ba20829a0014f0c9484
 } from '@angular/material';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
-
 import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login.component';
 import { HomeComponent } from './home/home.component';
@@ -32,7 +27,6 @@ import { RecordsComponent } from './records/records.component';
 import { HeaderComponent } from './header/header.component';
 import { InventoryComponent } from './inventory/inventory.component';
 import { ManagerActionsComponent } from './manager-actions/manager-actions.component';
-
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgxMaskModule } from 'ngx-mask';
 
@@ -77,14 +71,11 @@ const routes: Routes = [
     NgbModule,
     ReactiveFormsModule,
     MatExpansionModule,
-<<<<<<< HEAD
-    NgxMaskModule.forRoot()
+    NgxMaskModule.forRoot(),
+    MatTableModule
   ],
   entryComponents: [
-    ManagerActionsComponent
-=======
-    MatRadioModule
->>>>>>> 254234dc2bfd279d77a67ba20829a0014f0c9484
+    ManagerActionsComponent,
   ],
   providers: [],
   bootstrap: [AppComponent]
