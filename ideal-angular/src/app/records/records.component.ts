4e5d51523351999a1ee86abfa3e8966e146862f3
Sagar Patadia <35351813+spatadia@users.noreply.github.com>
2019-02-27 04:22:16 -0500

    Worked on adding customer.... WIP

--- ideal-angular/src/app/records/records.component.ts
+++ ideal-angular/src/app/records/records.component.ts
@@ -20,10 +20,18 @@ import { DialogEntryCustomerComponent } from '../dialog-entry-customer/dialog-en
 export class RecordsComponent implements OnInit {
 
   dialogEntryRef: MatDialogRef<DialogEntryCustomerComponent>;
+  searchForm: FormGroup;
+  isLoading = false;
+  checked = false;
+  customers: Customer[] = [];
+  totalCustomers = 0;
+  private customersSub: Subscription;
+  dataSource: MatTableDataSource<Customer>;
 
   constructor(
     private titleService: Title,
-    private dialog: MatDialog
+    private dialog: MatDialog,
+    private customerService: CustomerService
   ) {}
 
   displayedColumns = [
@@ -185,14 +193,45 @@ export class RecordsComponent implements OnInit {
     },
   ];
 
-  dataSource = new MatTableDataSource(this.customerData);
+  // dataSource = new MatTableDataSource(this.customerData);
 
   applyFilter(filterValue: string) {
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
+
   ngOnInit() {
-    this.titleService.setTitle('Customer Records | iDealCars');
+    if (this.checked) {
+      this.isLoading = true;
+    } else {
+      this.isLoading = false;
+    }
+  this.titleService.setTitle('Vehicle Inventory | iDealCars');
+  this.searchForm = new FormGroup({
+    'Year': new FormControl(null, {
+      validators: [Validators.required]
+    }),
+    'Make': new FormControl(null, {
+      validators: [Validators.required]
+    }),
+    'Model': new FormControl(null, {
+      validators: [Validators.required]
+    }),
+    'Color': new FormControl(null, {
+      validators: [Validators.required]
+    }),
+
+  });
+  this.getPeople();
+}
+
+getPeople(): void {
+  this.customerService.getCustomers();
+  this.customerService.getCustomerUpdateListener()
+    .subscribe((customerData: { customers: Customer[] }) => {
+      this.customers = customerData.customers;
+      this.dataSource = new MatTableDataSource(this.customers);
+    });
   }
 
   openDialogEntry() {
