4dd0b28167829888c30e48fe8b872bd62d755706
Jerrod Mathis <jerrodmathis95@gmail.com>
2019-03-04 00:53:33 -0500

    updating files for deployment

--- ideal-api/routes/user.js
+++ ideal-api/routes/user.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const multer = require('multer');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
