f6c6a402fa1e7c0f1da1c8aba1d128174a92210a
Jerrod Mathis <jerrodmathis95@gmail.com>
2019-04-07 23:32:42 -0400

    updated alert styling

--- ideal-angular/src/app/auth/alert/alert.component.ts
+++ ideal-angular/src/app/auth/alert/alert.component.ts
@@ -5,7 +5,8 @@ import { AlertService } from '../../../services/alert.service';
 
 @Component({
   selector: 'app-alert',
-  templateUrl: 'alert.component.html'
+  templateUrl: 'alert.component.html',
+  styleUrls: ['alert.component.css'],
 })
 export class AlertComponent implements OnInit, OnDestroy {
 
